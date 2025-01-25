4fc8f2cc09eb5fe163c683f723802615271009db
AleGrimaldi <grimaldialejandro5@gmail.com>
2025-01-25 16:34:44 -0300

    Se anulo un endpoint

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/admin', (req, res) => {
   });
 });
 
-app.get('/alumno', (req, res) => {
+/*app.get('/alumno', (req, res) => {
   const query = 'select id_cliente, apellido, nombreCliente, telefono from Cliente;';
   connection.query(query, (error, results) => {
     if (error) {
@@ -49,7 +49,7 @@ app.get('/alumno', (req, res) => {
     res.status(200).json(results);
   });
 });
-
+*/
 
 app.post('/agregar_usuario', (req, res) => {
   const datos = req.body;
